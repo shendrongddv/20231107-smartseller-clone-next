9ef7aef3ff61c527d754ae25b15e6dd5ca4ce439
Kang Shend Rong <shendrongddv@gmail.com>
2023-11-08 17:05:07 +0700

    20231108: Logo Clouds

--- app/(home)/_components/logo-clouds.tsx
+++ app/(home)/_components/logo-clouds.tsx
@@ -4,9 +4,9 @@ import { dataLogoClouds } from "../data";
 
 const LogoCloudsSection = () => {
   return (
-    <section className="px-4 py-24">
+    <section className="py-24 md:px-4">
       <div className="container flex flex-col items-center gap-8">
-        <h2 className="text-center text-xl font-bold text-foreground md:w-1/2">
+        <h2 className="text-center text-xl font-bold text-foreground max-md:mx-2 md:w-1/2">
           90rb++ menggunakan SmartSeller untuk kemudahan mengelola bisnis online
           mereka
         </h2>
@@ -25,7 +25,7 @@ const LogoCloudsSection = () => {
           ))}
         </Marquee>
 
-        <Marquee direction="right">
+        <Marquee direction="right" className="-mt-4">
           {dataLogoClouds?.slice(6, 12)?.map((item) => (
             <figure key={item.id} className="px-6 py-4 md:px-12">
               <Image
